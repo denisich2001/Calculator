0792fe776f9687605419ea4412314c300f0a057a
denisich2001 <denisich2001@mail.ru>
2021-02-23 21:22:54 +0300

    Application interface is loading without errors

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button digits = new Button(), add, subtract, multiply, equal, divide, erase;
-    EditText result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
